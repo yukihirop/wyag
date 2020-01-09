4e83dec2b6c78104c027b28aaba32281c31e1ac1
yukihirop <te108186@gmail.com>
2020-01-09 18:29:32 +0900

    Reading && Writing objects

--- libwyag.py
+++ libwyag.py
@@ -191,3 +191,51 @@ whatever it takes to convert it into a meaningful representation. What exactly m
     raise Exception("Unimplemented!")
 
 
+def object_read(repo, sha):
+  """Read object object_id from Git repository repo. Return a
+  GitObject whose exact type depends on the object."""
+
+  path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+  with open(path, "rb") as f:
+    raw = zlib.decompress(f.read())
+
+    # Read object type
+    x = raw.find(b' ')
+    fmt = raw[0:x]
+
+    # Read and validate object size
+    y = raw.find(b'\x00', x)
+    size = int(raw[x:y].decode("ascii"))
+    if size != len(raw) - y - 1:
+      raise Exception("Malformed object {0}: bad length".format(sha))
+
+    # Pick constructor
+    if   fmt==b'commit'  : c=GitCommit
+    elif fmt==b'tree'    : c=GitTree
+    elif fmt==b'tag'     : c=GitTag
+    elif fmt==b'blob'    : c=GitBlob
+    else:
+      raise Exception("Unknown type %s for object %s".format(fmt.decode("ascii"), sha))
+
+    # Call constructore and return object
+    return c(repo, raw[y+1:])
+
+
+def object_write(obj, actually_write=True):
+  # Serialize object data
+  data = obj.serialize()
+  # Add header
+  result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+  # Compute hash
+  sha = hashlib.sha1(result).hexdigest()
+
+  if actually_write:
+    # Compute path
+    path=repo_file(obj.repo, "objects", sha[0:2], sha[2:], mkdir=actually_write)
+
+    with open(path, 'wb') as f:
+      # Compress and Write
+      f.write(zlib.compress(result))
+
+  return sha
